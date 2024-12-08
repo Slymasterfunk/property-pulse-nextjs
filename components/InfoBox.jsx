840cde19eb6f119d0003485d619c75936bde5de4
Sly <slymasterfunk@gmail.com>
2024-12-08 06:44:42 -0600

    created Not Found, Loader, and property components

--- components/InfoBox.jsx
+++ components/InfoBox.jsx
@@ -1,4 +1,4 @@
-const InfoBoxes = ({
+const InfoBox = ({
   heading,
   backgroundColor = 'bg-gray-100',
   textColor = 'text-gray-800',
@@ -6,12 +6,12 @@ const InfoBoxes = ({
   children
 }) => {
   return (
-    <div class={`${backgroundColor} p-6 rounded-lg shadow-md`}>
-      <h2 class={`${textColor} text-2xl font-bold`}>{heading}</h2>
-      <p class={`${textColor} mt-2 mb-4`}>{children}</p>
+    <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
+      <h2 className={`${textColor} text-2xl font-bold`}>{heading}</h2>
+      <p className={`${textColor} mt-2 mb-4`}>{children}</p>
       <a
         href={buttonInfo.link}
-        class={`${buttonInfo.backgroundColor} inline-block text-white rounded-lg px-4 py-2 hover:bg-gray-700`}
+        className={`${buttonInfo.backgroundColor} inline-block text-white rounded-lg px-4 py-2 hover:bg-gray-700`}
       >
         {buttonInfo.text}
       </a>
@@ -19,4 +19,4 @@ const InfoBoxes = ({
   )
 }
 
-export default InfoBoxes
+export default InfoBox
